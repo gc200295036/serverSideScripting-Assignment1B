c48f38de79879fa342384ddddf492341ad281dae
gc200295036 <nickbrazda95@hotmail.com>
2020-10-21 18:08:35 -0400

    added ItemsController CRUD

---
+++ Data/Migrations/20201021215800_UpdateCategoryTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace serverSideScripting_Assignment1B.Data.Migrations
+{
+    public partial class UpdateCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Categories",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "FirstName",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
