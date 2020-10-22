d12d2d74b9d22733bc8367152670bb0da74e0e3c
gc200295036 <nickbrazda95@hotmail.com>
2020-10-21 22:30:45 -0400

    update

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace serverSideScripting_Assignment1B.Models
     public class Category
     {
         public int CategoryId { get; set; } // CategoryId = Primary Key for Table
-
+        // I had to lookup how to column name, I found that using data annotations will change the display for the column name, This is awesome for when I need to change category names without restarting a whole new database.
+        [Display(Name = "Name")]
         public string FirstName { get; set; }
 
         // Create Category and Item Model relationship to show item list within this category.
--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,12 +11,14 @@ namespace serverSideScripting_Assignment1B.Models
         //start of creating my table this will give a unique ItemId to a persons contact information.
         // This would help a website keep track of what item is linked to a specific person.
         public int ItemId { get; set; } // ItemId = Primary Key for Table
+       
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
         public string Email { get; set; }
         //Foreign Key CategoryId Referenced, must be referenced here so it is useable in applicationdbcontext.cs for this code to work .HasConstraintName("FK_Items_CategoryId");
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
         // Category creates relationship with our model category so we can show our list of items
         public Category Category { get; set; }
